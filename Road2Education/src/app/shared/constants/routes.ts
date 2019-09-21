af6f6d59eef02f33a506d8d5544c1d87aabeaa83
Marian Salvan <marian.salvan@gmail.com>
2019-09-22 00:45:28 +0300

    added AngularFire module

--- Road2Education/src/app/shared/constants/routes.ts
+++ Road2Education/src/app/shared/constants/routes.ts
@@ -1,12 +1,12 @@
 import { Routes } from '@angular/router';
-import { MainDashboardComponent } from '../main-components/main-dashboard/main-dashboard.component';
-import { DriverDashboardComponent } from '../modules/driver/driver-dashboard/driver-dashboard.component';
-import { RoleAuthentificationGuard } from '../services/guards/role-authentification.guard';
-import { AdminDashboardComponent } from '../modules/admin/admin-dashboard/admin-dashboard.component';
 import { Roles } from './roles';
-import { StudentDashboardComponent } from '../modules/student/student-dashboard/student-dashboard.component';
-import { LoginComponent } from '../modules/authentification/login/login.component';
-import { RegisterComponent } from '../modules/authentification/register/register.component';
+import { MainDashboardComponent } from 'src/app/main-components/main-dashboard/main-dashboard.component';
+import { LoginComponent } from 'src/app/modules/authentification/login/login.component';
+import { RegisterComponent } from 'src/app/modules/authentification/register/register.component';
+import { AdminDashboardComponent } from 'src/app/modules/admin/admin-dashboard/admin-dashboard.component';
+import { RoleAuthentificationGuard } from 'src/app/services/guards/role-authentification.guard';
+import { DriverDashboardComponent } from 'src/app/modules/driver/driver-dashboard/driver-dashboard.component';
+import { StudentDashboardComponent } from 'src/app/modules/student/student-dashboard/student-dashboard.component';
 
 export const ROUTES: Routes = [
   { path: '', component: MainDashboardComponent },
