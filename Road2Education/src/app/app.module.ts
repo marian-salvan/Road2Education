7a3195209aea5be3e0db4f7937145909fb538a30
Marian Salvan <marian.salvan@gmail.com>
2019-09-21 21:36:08 +0300

    added angular material

--- Road2Education/src/app/app.module.ts
+++ Road2Education/src/app/app.module.ts
@@ -2,17 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatRippleModule, MatIconModule } from '@angular/material';
+import { AuthentificationModule } from './modules/authentification/authentification.module';
+import { LoginComponent } from './modules/authentification/login/login.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatRippleModule,
+    MatIconModule,
+
+    // application modules
+    AuthentificationModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [LoginComponent]
 })
 export class AppModule { }
---
+++ Road2Education/src/app/modules/authentification/authentification.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AuthentificationRoutingModule } from './authentification-routing.module';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+
+@NgModule({
+  declarations: [
+    LoginComponent, 
+    RegisterComponent
+  ],
+  imports: [
+    CommonModule,
+    AuthentificationRoutingModule
+  ]
+})
+export class AuthentificationModule { }
