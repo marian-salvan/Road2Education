8032616abd5d3942ea55290ddea814b22d9a385b
Marian Salvan <marian.salvan@gmail.com>
2019-10-19 11:49:41 +0300

    completed offers/requests implementation + other small fixes

--- Road2Education/src/app/modules/base-components/request-view/request-view.component.ts
+++ Road2Education/src/app/modules/base-components/request-view/request-view.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { RouteRequest } from 'src/app/models/route';
+import { UserService } from 'src/app/services/user/user.service';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { DatePipe } from '@angular/common';
+import { User } from 'src/app/models/users';
+import { UserInformationModalComponent } from '../user-information-modal/user-information-modal.component';
 
 @Component({
   selector: 'app-request-view',
@@ -10,9 +15,29 @@ export class RequestViewComponent implements OnInit {
 
   @Input() routeRequest: RouteRequest;
 
-  constructor() { }
+  requestPoster: User;
+  dateTimestamp: number;
+
+  constructor(public datepipe: DatePipe, 
+              private modalService: NgbModal,
+              private userService: UserService) { }
 
   ngOnInit() {
+    this.getOfferPoster();
+  }
+  
+  ngOnChanges() {
+    this.getOfferPoster();
   }
 
+  private getOfferPoster() {
+    this.userService.getUser(this.routeRequest.student).subscribe(user => {
+      this.requestPoster = user;
+    });
+  }
+
+  openUserInformationDialog() {
+    const modalRef = this.modalService.open(UserInformationModalComponent);
+    modalRef.componentInstance.user = this.requestPoster;
+  }
 }
